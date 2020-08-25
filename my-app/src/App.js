beed9677d6dbf134ae5c9df5cfc1139006696c85
Aritro Pal Choudhury <aritropc@gmail.com>
2020-08-25 14:02:49 +0530

    react app updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+//import { makeStyles } from '@material-ui/core/styles';
+//import Button from '@material-ui/core/Button';
+
+import robot from './robot.png';
 import './App.css';
 
+//UI Button for Start 
+/*const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));*/
+
+//main body containing all elements
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="row">
+          <div className="column left" >
+            <img src={robot} alt="robot" className="robot"></img>
+          </div>          
+          <div className="column right" >
+            <h1>Begin your journey <br /> with Machine Learning here with us !</h1>            
+          </div>
+        </div>
+        <div className="column left">
+          <button onClick={window.open("www.google.co.in", '_blank')} className="start" width={100} >Start</button>
+        </div>
+
       </header>
     </div>
   );
